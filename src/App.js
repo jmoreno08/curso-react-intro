740707eb581e4bbd5fae9a6d649a5d9efeab39e4
Jonathan Moreno <morenoelectro@gmail.com>
2024-06-05 05:08:44 -0500

    add localStorage

--- src/App.js
+++ src/App.js
@@ -4,35 +4,59 @@ import { TodoSearch } from "./TodoSearch";
 import { TodoList } from "./TodoList";
 import { TodoItem } from "./TodoItem";
 import { CreateTodoButton } from "./CreateTodoButton";
-const defaultTodos = [
-  { text: "Cortar cebolla", completed: true },
-  { text: "Tomar el Curso de Intro a React.js", completed: false },
-  { text: "Llorar con la Llorona", completed: false },
-  { text: "LALALALALA", completed: false },
-  { text: "Usar estados derivados", completed: true },
-];
+
+// const defaultTodos = [
+//   { text: "Cortar cebolla", completed: true },
+//   { text: "Tomar el Curso de Intro a React.js", completed: false },
+//   { text: "Llorar con la Llorona", completed: false },
+//   { text: "LALALALALA", completed: false },
+//   { text: "Usar estados derivados", completed: true },
+// ];
+
+//localStorage.setItem('TODOS_V1',defaultTodos)
+
+//localStorage.removeItem('TODO_V1')
+
 function App() {
-  const [todos, setTodos] = React.useState(defaultTodos);
+  
+  const localStorageTodos = localStorage.getItem('TODOS_V1');
+
+  let parsedTodos ;
+
+  if(!localStorageTodos){
+    localStorage.setItem('TODOS_V1',JSON.stringify([]));
+    parsedTodos=[];
+  }else{
+    parsedTodos = JSON.parse(localStorageTodos)
+  }
+
+  const [todos, setTodos] = React.useState(parsedTodos);
   const [searchValue, setSearchValue] = React.useState("");
+
   const completedTodos = todos.filter((todo) => !!todo.completed).length;
+
   const totalTodos = todos.length;
+
   const searchedTodos = todos.filter((todo) => {
     const todoText = todo.text.toLowerCase();
     const searchText = searchValue.toLowerCase();
     return todoText.includes(searchText);
   });
+
   const completeTodo = (text) => {
     const newTodos = [...todos];
     const todoIndex = newTodos.findIndex((todo) => todo.text == text);
     newTodos[todoIndex].completed = true;
     setTodos(newTodos);
   };
+
   const deleteTodo = (text) => {
     const newTodos = [...todos];
     const todoIndex = newTodos.findIndex((todo) => todo.text == text);
     newTodos.splice(todoIndex, 1);
     setTodos(newTodos);
   };
+  
   return (
     <>
       <TodoCounter completed={completedTodos} total={totalTodos} />
